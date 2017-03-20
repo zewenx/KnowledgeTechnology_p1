9385f40b620d9ba3011d11906a2522a7fc72ff38
Francis <zw.final@gmail.com>
2017-03-20 22:55:58 +1100

    include some libs

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,23 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("s");
+		try {
+			List <String> strs = FileUtils.readLines(new File("testFiles/train.txt"));
+			for (String string : strs) {
+				System.out.println(string);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
